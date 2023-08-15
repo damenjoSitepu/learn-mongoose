58df59e1e23cc77ad395cdfc1ded79490c917cf4
Damenjo Sitepu <damenstory@gmail.com>
2023-08-15 14:05:37 +0700

    Feat: create data with new or create keyword

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,19 @@ import ToolSchema from "./schemas/Tool.schema";
 
 mongoose.connect("mongodb://localhost/school");
 
-/**
- * Create Tool
- */
-async function createTool(): Promise<void> {
-    // const tool = new ToolSchema({
-    //     name: "Wand",
-    //     durability: 1000,
-    //     isActive: true
-    // });
-    // await tool.save();
+// New keyword
+async function createToolWithNewKeyword(): Promise<void> {
+    const tool = new ToolSchema({
+        name: "Wand",
+        durability: 1000,
+        isActive: true
+    });
+    await tool.save();
+
+}
+
+// Create keyword
+async function createToolWithCreateKeyword(): Promise<void> {
     await ToolSchema.create({
         name: "Wand",
         durability: 1000,
